3288e5335465a2d07b8123853fe69a9873db2263
Giang Van <noname7652259@gmail.com>
2019-12-28 14:10:41 +0700

    first commit

--- autopush.cmd
+++ autopush.cmd
@@ -3,5 +3,5 @@ git init
 git add .
 git commit -m "first commit"
 git remote add origin https://github.com/GiangVan/Light-API.git
-git push -u origin test
+git push -u origin master
 git config http.sslVerify true
